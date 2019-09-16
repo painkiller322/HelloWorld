cba2184e4f0bd0d9ea63fa4ef51c8d1613a0231e
User <User@YD03-20>
2019-09-16 18:00:29 +0900

    20190916_2

--- HelloWorld/src/hello/gugudan.java
+++ HelloWorld/src/hello/gugudan.java
@@ -2,10 +2,24 @@ package hello;
 
 public class gugudan {
 	public static void main(String[] args) {
+/*
 		for (int i = 1; i < 10; i++) {
 			for (int j = 2; j < 10; j++) {
 				System.out.print(j + "*" + i + "=" + j * i + " ");
 			}System.out.println();
 		}
+	}
+	
+	*/
+		//while 구구단
+	int a = 1;
+	while (a < 10) {
+		int j = 2;
+		while (j < 10) {
+			System.out.print(j + "*" + a + "=" + (j * a)+" ");
+			j++;
+		}System.out.println();
+		a++;
+	}
 	}
 }
