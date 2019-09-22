0486f26b647bc230cc5a4fc336f80e7e4644fef9
Administrator <Administrator@DESKTOP-MV7FKA1>
2019-09-22 11:31:18 +0900

    20190922

--- HelloWorld/src/exercise/execise05_08.java
+++ HelloWorld/src/exercise/execise05_08.java
@@ -2,17 +2,20 @@ package exercise;
 
 public class execise05_08 {
 	public static void main(String[] args) {
-		int[][] array= {
-				{95,86},
-				{83,92,96},
-				{78,83,93,87,88}
-		};
-		int sum=0;
-		double avg=0.0;
-		
-		//작성
-				
-		System.out.println("sum: "+sum);
-		System.out.println("avg: "+avg);
+		int[][] array = { { 95, 86 }, { 83, 92, 96 }, { 78, 83, 93, 87, 88 } };
+		int sum = 0;
+		double avg = 0.0;
+
+		// 작성
+		int c = 0;
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[i].length; j++) {
+				sum += array[i][j];
+				c++;
+				avg = (double) sum / c;
+			}
+		}
+		System.out.println("sum: " + sum);
+		System.out.println("avg: " + avg);
 	}
 }
